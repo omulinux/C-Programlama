66f439bbd64f4cd607a7057fd064e7f829820517
omulinux <omulinux.groups@gmail.com>
2014-12-13 20:55:54 +0200

    Create If_Kullanimi.c

---
+++ If_Kullanimi.c
@@ -0,0 +1,17 @@
+// ifDeyimi.c
+
+# include <stdio.h>
+
+int main() {
+    int puan = 93;
+    
+    if (puan>=60) {
+                  printf("tebrikler sinavi gectiniz!\n");
+                  printf("puaniniz: %d\n\n", puan);
+                  
+                  }
+                  
+    printf("programi sonlandir..\n");
+    return 0;
+    
+}
